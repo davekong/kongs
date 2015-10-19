efe709bc6e051d2d874c3ce3a07231dfb1a6257a
davekongs <573330500@qq.com>
2015-10-19 13:25:52 +0800

    check and push

--- src/com/kongs/test/TestForGit.java
+++ src/com/kongs/test/TestForGit.java
@@ -5,6 +5,7 @@ public class TestForGit {
 	public static void main(String[] args) {
 		System.out.println("first github program.");
 		System.out.println("just test for push code.");
+		System.out.println("just test for checkout and push code.");
 	}
 
 }
